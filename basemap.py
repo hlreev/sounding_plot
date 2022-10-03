e6695cd989435ff3cdbf55957577ca27d5488a55
hlreev <hunter.reeves@noaa.gov>
2022-10-02 21:27:06 -0500

    gotta figure out this basemap issue

--- basemap.py
+++ basemap.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python
-from mpl_toolkits.basemap import Basemap
+import os
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from pyproj import geodesic_version_str
+os.environ['PROJ_LIB'] = '/Users/hunlr/anaconda3/envs/cartopy_env/Library/share/proj'
+from mpl_toolkits.basemap import Basemap
 
-# input desired coordinates
-my_coords = [38.9719980,-76.9219820]
+# Read in data for usage
+data = pd.read_csv("data/20221001_00z.csv")
+
+# Parse out the data for usage
+lats = data["latitude"]
+lons = data["longitude"]
+alts = data['geometric_altitude']
 
 # How much to zoom from coordinates (in degrees)
-zoom_scale = 1
+lat_zoom_scale = 7
+lon_zoom_scale = 14
 
 # Setup the bounding box for the zoom and bounds of the map
-bbox = [my_coords[0]-zoom_scale,my_coords[0]+zoom_scale,\
-        my_coords[1]-zoom_scale,my_coords[1]+zoom_scale]
+bbox = [np.min(lats)-lat_zoom_scale,np.max(lats)+lat_zoom_scale,\
+        np.min(lons)-lon_zoom_scale,np.max(lons)+lon_zoom_scale]
 
 plt.figure(figsize=(12,6))
 # Define the projection, scale, the corners of the map, and the resolution.
 m = Basemap(projection='merc',llcrnrlat=bbox[0],urcrnrlat=bbox[1],\
             llcrnrlon=bbox[2],urcrnrlon=bbox[3],lat_ts=10,resolution='i')
 
-# Draw coastlines and fill continents and water with color
+# Draw the world around us
+m.fillcontinents(color='#69b2a2',lake_color='#A6CAE0') 
 m.drawcoastlines()
-m.fillcontinents(color='peru',lake_color='dodgerblue')
+m.drawcountries(color='grey', linewidth=1)
+m.drawstates(color='lightgrey', linewidth=1)
+
 
 # draw parallels, meridians, and color boundaries
 m.drawparallels(np.arange(bbox[0],bbox[1],(bbox[1]-bbox[0])/5),labels=[1,0,0,0])
@@ -28,8 +41,8 @@ m.drawmeridians(np.arange(bbox[2],bbox[3],(bbox[3]-bbox[2])/5),labels=[0,0,0,1],
 m.drawmapboundary(fill_color='dodgerblue')
 
 # build and plot coordinates onto map
-x,y = m(my_coords[1],my_coords[0])
-m.plot(x,y,marker='D',color='r')
-plt.title("Geographic Point Test")
-plt.savefig('coordinate_test.png', format='png', dpi=500)
+x,y = m(lons,lats)
+m.plot(x,y,'D',markersize=5)
+plt.title("2D Balloon Trajectory")
+plt.savefig('2d-trajectory.png', format='png', dpi=500)
 plt.show()
